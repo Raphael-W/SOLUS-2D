ba9499d85e6a1f1678feaed4be36c0dca8a8b455
Raphael <raphaelwreford@hotmail.com>
2023-06-27 16:41:37 +0100

    Tile Setup

--- SOLUS 2D/Assets/Scripts/Generation.cs
+++ SOLUS 2D/Assets/Scripts/Generation.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Generation : MonoBehaviour
 {
+    public Tilemap tilemap;
+    public Tile mainTile;
+    public Vector3Int tilePos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tilePos = new Vector3Int(0, 0, 0);
+        //tilemap.SetTile(tilePos, mainTile);
+
+        Debug.Log(tilemap.HasTile(tilePos));
     }
 
     // Update is called once per frame

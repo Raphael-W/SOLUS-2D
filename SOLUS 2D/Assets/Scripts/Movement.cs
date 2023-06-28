b9386066a3d95e6a2e275535019be1451e2371c6
Raphael <raphaelwreford@hotmail.com>
2023-06-28 11:14:08 +0100

    Added Rocket Movement
    MUST ADD SPEED LIMIT!!

---
+++ SOLUS 2D/Assets/Scripts/Movement.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Movement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    private MvInputKey Key;
+    private float speed;
+    private Vector2 oldPosition;
+
+
+
+    [SerializeField] public float mainThrust = 400000f; //1000
+    //[SerializeField] public float backgroundThrust = 10000;
+    [SerializeField] public float rotationThrust = 100f;
+
+
+    enum MvInputKey {
+        Key_Neutral = 0,
+        Key_Left = 1,
+        Key_Right = 2,
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+
+        Key = MvInputKey.Key_Neutral;
+
+        if (Input.GetKey(KeyCode.A)) {
+            Key = Key | MvInputKey.Key_Left;
+
+        }
+    
+        if (Input.GetKey(KeyCode.D)) {
+            Key = Key | MvInputKey.Key_Right;
+
+        }
+
+
+
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+
+        ProcessRotation();
+        //rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+
+        speed = Vector2.Distance(oldPosition, transform.position) * 100f;
+        oldPosition = transform.position;
+        Debug.Log("Speed: " + speed.ToString("F2"));
+
+
+
+    }
+
+
+
+    void ProcessRotation()
+    {
+        //textLabel.text = "";
+
+        if ((Key & MvInputKey.Key_Left)!=0) {
+            RotatePlayer(rotationThrust);
+        }
+        if ((Key & MvInputKey.Key_Right)!=0) {
+            RotatePlayer(-rotationThrust);
+        }
+        if (Key != MvInputKey.Key_Neutral) {
+
+            if ((Key & MvInputKey.Key_Left)!=0 && (Key & MvInputKey.Key_Right)!=0) {
+                rb.AddRelativeForce(Vector2.up * mainThrust * Time.deltaTime);
+            }
+
+            else {
+                rb.AddRelativeForce(Vector2.up * (mainThrust / 2) * Time.deltaTime);
+
+            }
+
+        }
+    }
+
+    private void RotatePlayer(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; //freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotationThisFrame);
+        rb.freezeRotation = false; //unfreezing rotation so the physics system can take over
+    }
+}

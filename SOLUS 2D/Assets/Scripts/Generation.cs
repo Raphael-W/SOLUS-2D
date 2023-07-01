b2cbfd4138837848d3a263279784cd1444d2f39b
Raphael Wreford <raphaelwreford@hotmail.com>
2023-07-01 10:50:52 +0100

    Random Seed

--- SOLUS 2D/Assets/Scripts/Generation.cs
+++ SOLUS 2D/Assets/Scripts/Generation.cs
@@ -23,10 +23,10 @@ public class Generation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        seed = Random.Range(1000000, 9999999);
+        seed = Random.Range(1000000, 9999999); //8024669
         Debug.Log("Seed: " + seed);
 
-        Random.InitState(8024669);
+        Random.InitState(seed);
 
         InitialiseTiles();
 

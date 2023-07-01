7c0c7a6768b0be91aac865cee0314c48b2d32be2
Raphael Wreford <raphaelwreford@hotmail.com>
2023-07-01 14:58:24 +0100

    Custom IP

--- SOLUS 2D/Assets/Netcode/JoinClient.cs
+++ SOLUS 2D/Assets/Netcode/JoinClient.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
 using Unity.Netcode;
-using UnityEngine.UI;
+using Unity.Netcode.Transports.UTP;
+//using MLAPI.Transports.UNET;
+using TMPro;
 
 public class JoinClient : MonoBehaviour
 {
-    public TextMesh IPField;
+    public TMP_InputField IPField;
     private string InputtedIP;
 
     public void JoinIP()
     {
         InputtedIP = IPField.text.ToString();
-        NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = InputtedIP;
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(InputtedIP, (ushort)6666);
 
         NetworkManager.Singleton.StartClient();
     }

74c6171fe7aa8be5f712f3e09e01e5f951f27d79
Raphael Wreford <raphaelwreford@hotmail.com>
2023-07-19 15:16:18 +0100

    Added NetworkBehaviour

--- SOLUS 2D/Assets/Scripts/BulletController.cs
+++ SOLUS 2D/Assets/Scripts/BulletController.cs
@@ -1,7 +1,7 @@
 using Unity.Netcode;
 using UnityEngine;
 
-public class BulletController : MonoBehaviour
+public class BulletController : NetworkBehaviour
 {
 
     public float MoveSpeed;
--- SOLUS 2D/Assets/Scripts/ServerManager.cs
+++ SOLUS 2D/Assets/Scripts/ServerManager.cs
@@ -79,7 +79,7 @@ public class ServerManager : NetworkBehaviour
     {
         //var clientId = serverRpcParams.Receive.SenderClientId;
         SpawnedBullet = Instantiate(BulletPrefab, position, rotation);
-        SpawnedBullet.GetComponent<NetworkObject>().SpawnWithOwnership(0);
+        SpawnedBullet.GetComponent<NetworkObject>().SpawnWithOwnership(ClientID);
         SpawnedBullet.GetComponent<BulletController>().Direction = direction;
         SpawnedBullet.GetComponent<BulletController>().shoot = true;
     }
